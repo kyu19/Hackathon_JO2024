3cd4843aca3c972f6d974e523a483a983e5c9087
ocherif <cherifoussama1996@gmail.com>
2024-05-29 10:27:47 +0200

    connexion base de données

--- backend/hackaton2024jo_back/app/main.py
+++ backend/hackaton2024jo_back/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from app.routers import athletes, medals, results, hosts
+from app.db import get_db_connection
+from app.routers import athletes, medals, results, hosts, data
 
 app = FastAPI()
 
@@ -7,7 +8,27 @@ app.include_router(athletes.router, prefix="/athletes", tags=["Athletes"])
 app.include_router(medals.router, prefix="/medals", tags=["Medals"])
 app.include_router(results.router, prefix="/results", tags=["Results"])
 app.include_router(hosts.router, prefix="/hosts", tags=["Hosts"])
+app.include_router(data.router, prefix="/data", tags=["Data"])
+# app.include_router(predictions.router, prefix="/predictions", tags=["Predictions"])
 
 @app.get("/")
 def read_root():
     return {"message": "Welcome to the Olympics API!"}
+
+@app.get("/test-db")
+def test_db_connection():
+    conn = get_db_connection()
+    if conn:
+        try:
+            cursor = conn.cursor(dictionary=True)
+            cursor.execute("SELECT * FROM olympic_hosts LIMIT 1")
+            result = cursor.fetchone()
+            conn.close()
+            if result:
+                return {"message": "Connexion réussie à la base de données", "sample_data": result}
+            else:
+                return {"message": "Connexion réussie à la base de données, mais aucune donnée n'a été trouvée"}
+        except Exception as e:
+            return {"message": f"Erreur lors de l'exécution de la requête: {str(e)}"}
+    else:
+        return {"message": "Échec de la connexion à la base de données"}
---
+++ backend/hackaton2024jo_back/app/routers/data.py
@@ -0,0 +1,41 @@
+from fastapi import APIRouter
+import pandas as pd
+from app.db import get_db_connection
+
+router = APIRouter()
+
+def query_to_dataframe(query, connection):
+    return pd.read_sql(query, connection)
+
+@router.get("/hosts")
+def get_hosts():
+    conn = get_db_connection()
+    if conn:
+        query = "SELECT * FROM olympic_hosts"
+        df = query_to_dataframe(query, conn)
+        conn.close()
+        return df.to_dict(orient='records')
+    else:
+        return {"error": "Failed to connect to the database"}
+
+@router.get("/medals")
+def get_medals():
+    conn = get_db_connection()
+    if conn:
+        query = "SELECT * FROM medals"
+        df = query_to_dataframe(query, conn)
+        conn.close()
+        return df.to_dict(orient='records')
+    else:
+        return {"error": "Failed to connect to the database"}
+
+@router.get("/athletes")
+def get_athletes():
+    conn = get_db_connection()
+    if conn:
+        query = "SELECT * FROM athletes"
+        df = query_to_dataframe(query, conn)
+        conn.close()
+        return df.to_dict(orient='records')
+    else:
+        return {"error": "Failed to connect to the database"}
